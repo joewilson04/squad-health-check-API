30c7510a37337df45b48f3e0a0880854f63eb670
joewilson04 <55708509+joewilson04@users.noreply.github.com>
2022-07-21 11:04:02 +0100

    configured project to run on docker

---
+++ code/asgi.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'code.settings')
+
+application = get_asgi_application()
