564ef18c36750d67869ecb88fa7584e8243bf46f
joewilson04 <55708509+joewilson04@users.noreply.github.com>
2022-07-27 11:38:19 +0100

    removed unnecessary imports in url files

--- main_app/urls.py
+++ main_app/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
-from django.views.generic.base import TemplateView
-from django.contrib.auth.views import LoginView
+
 
 
 urlpatterns = [
