ccd136e18619085407d2849e9c6bdbe764473a4c
joewilson04 <55708509+joewilson04@users.noreply.github.com>
2022-07-27 12:06:56 +0100

    created a functional homepage and back buttons on the two main login forms

--- main_app/urls.py
+++ main_app/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls, name='admin'),
     path('scrum/', include('scrum_app.urls')),
     path('squad/', include('squad_app.urls')),
